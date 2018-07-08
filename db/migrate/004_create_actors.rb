ef192162ca745cf5b1cde6f204df0457bb6344f9
Cristina Lugo <cmlugoce@syr.edu>
2018-07-08 02:16:35 +0000

    automatically backed up by learn

--- db/migrate/004_create_actors.rb
+++ db/migrate/004_create_actors.rb
@@ -1,9 +1,8 @@
 lass CreateActors < ActiveRecord::Migration[4.2]
-+  def change
-+    create_table :actors do |t|
-+      #primary key of :id is created for us!
-+      t.string :first_name
-+      t.string :last_name
-+    end
-+  end
-+end
+  def change
+    create_table :actors do |t|
+      t.string :first_name
+      t.string :last_name
+    end
+  end
+end
